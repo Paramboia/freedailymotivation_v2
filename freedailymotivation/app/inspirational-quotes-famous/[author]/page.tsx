f3e138f0762443302a8eb936679ee1f0d8f486a7
Miguel Joao Macedo Parente <miguel.joao.parente@outlook.com>
2024-10-31 01:34:32 +0100

    fetch author name

--- freedailymotivation/app/inspirational-quotes-famous/[author]/page.tsx
+++ freedailymotivation/app/inspirational-quotes-famous/[author]/page.tsx
@@ -22,7 +22,7 @@ async function getAuthorQuotes(authorName: string) {
     .select(`
       id,
       quote_text,
-      authors!inner (
+      authors (
         author_name
       )
     `)
@@ -36,7 +36,7 @@ async function getAuthorQuotes(authorName: string) {
   return data.map(item => ({
     id: item.id,
     text: item.quote_text,
-    author: item.authors[0]?.author_name || 'Unknown Author',
+    author: item.authors?.author_name || 'Unknown Author',
     likes: 0,
     category: '',
     dislikes: 0
