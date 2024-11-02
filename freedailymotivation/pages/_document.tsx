9dbc26f2d546b23458f5546c7bd02139a5b6dfe3
Miguel Joao Macedo Parente <miguel.joao.parente@outlook.com>
2024-11-02 02:04:59 +0100

    gtm try

--- freedailymotivation/pages/_document.tsx
+++ freedailymotivation/pages/_document.tsx
@@ -6,16 +6,13 @@ export default function Document() {
     <Html lang="en">
       <Head>
         {/* Google Tag Manager */}
-        <Script
-          strategy="beforeInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=GTM-PQ8LSCWN"
-        />
-        <Script id="google-analytics" strategy="beforeInteractive">
+        <Script id="google-tag-manager" strategy="beforeInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'GTM-PQ8LSCWN');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PQ8LSCWN');
           `}
         </Script>
         {/* End Google Tag Manager */}
@@ -27,15 +24,6 @@ export default function Document() {
           height="0" width="0" style="display:none;visibility:hidden"></iframe>`
         }} />
         {/* End Google Tag Manager (noscript) */}
-        <Script id="google-tag-manager" strategy="beforeInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PQ8LSCWN');
-          `}
-        </Script>
         <Main />
         <NextScript />
       </body>
