a88b3ce963abfc24f32baf4ab5928b9c1a145957
Miguel Joao Macedo Parente <miguel.joao.parente@outlook.com>
2024-11-03 01:05:38 +0100

    fix vercel deploy

--- freedailymotivation/app/layout.tsx
+++ freedailymotivation/app/layout.tsx
@@ -6,6 +6,7 @@ import { ClerkProvider } from '@clerk/nextjs';
 import SiteHeader from "@/components/SiteHeader";
 import dynamic from 'next/dynamic';
 import { Analytics } from "@vercel/analytics/react";
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -48,8 +49,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PQ8LSCWN');
+          `}
+        </Script>
+      </head>
       <ClerkProvider>
         <body className={inter.className}>
+          {/* Google Tag Manager (noscript) */}
+          <noscript dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PQ8LSCWN"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+          }} />
+          {/* End Google Tag Manager (noscript) */}
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <div className="min-h-screen flex flex-col transition-colors duration-300 bg-gradient-to-br from-purple-400 to-pink-400 dark:from-black dark:to-zinc-900">
               <SiteHeader />
@@ -60,6 +78,7 @@ export default function RootLayout({
               </ErrorBoundary>
             </div>
           </ThemeProvider>
+          <Analytics />
         </body>
       </ClerkProvider>
     </html>
