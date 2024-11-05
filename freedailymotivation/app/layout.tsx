86f5ef2809bdcfcd51abd1e2e9f4642bd3981b8e
Miguel Joao Macedo Parente <miguel.joao.parente@outlook.com>
2024-11-05 15:33:43 +0100

    fix vercel deploy

--- freedailymotivation/app/layout.tsx
+++ freedailymotivation/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/contexts/theme-context";
 import { ClerkProvider } from '@clerk/nextjs';
