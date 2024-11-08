3dfe94fe6ae97e312c9dcb46d22353cde1e4d064
Miguel Joao Macedo Parente <miguel.joao.parente@outlook.com>
2024-11-08 10:43:42 +0100

    To make the suggestions disappear once a user selects one of the options

--- freedailymotivation/components/SearchBar.tsx
+++ freedailymotivation/components/SearchBar.tsx
@@ -36,6 +36,7 @@ export default function SearchBar() {
 
   const handleSelect = (authorName: string) => {
     setQuery(authorName);
+    setSuggestions([]);
     const slug = authorName.toLowerCase().replace(/\s+/g, '-');
     router.push(`/inspirational-quotes-famous/${slug}`);
   };
