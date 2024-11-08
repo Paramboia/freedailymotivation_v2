bebddf01cac40ddf1b887f82d6f977655ab2ee8e
Miguel Joao Macedo Parente <miguel.joao.parente@outlook.com>
2024-11-08 01:15:03 +0100

    footer good in all website

--- freedailymotivation/app/inspirational-quotes-famous/page.tsx
+++ freedailymotivation/app/inspirational-quotes-famous/page.tsx
@@ -63,15 +63,19 @@ export default async function InspirationalQuotesFamous() {
           ))}
         </div>
       </div>
-      <footer className="flex justify-between items-center p-4 text-sm text-white dark:text-gray-300">
-        <span className="flex-grow text-center">© 2024 Free Daily Motivation. All rights reserved.</span>
-        <div className="flex items-center">
-          <Link href="https://www.facebook.com/people/Free-Daily-Motivation/61566119962164/" className="flex items-center text-blue-600 hover:underline">
-            <Facebook className="h-5 w-5 mr-1" />
-            Join the Community
-          </Link>
-        </div>
-      </footer>
+      <footer className="flex justify-center items-center p-4 text-sm text-white dark:text-gray-300 relative">
+          <div className="text-center">
+            <span>© 2024 Free Daily Motivation.</span>
+            <br />
+            <span>All rights reserved.</span>
+          </div>
+          <div className="absolute right-4">
+            <Link href="https://www.facebook.com/people/Free-Daily-Motivation/61566119962164/" className="flex items-center text-blue-600 hover:underline">
+              <Facebook className="h-5 w-5 mr-1" />
+              Join the Community
+            </Link>
+          </div>
+        </footer>
     </div>
   );
 }
--- freedailymotivation/app/page.tsx
+++ freedailymotivation/app/page.tsx
@@ -87,9 +87,13 @@ export default function Home() {
             </div>
           )}
         </main>
-        <footer className="flex justify-between items-center p-4 text-sm text-white dark:text-gray-300">
-          <span className="flex-grow text-center">© 2024 Free Daily Motivation. All rights reserved.</span>
-          <div className="flex items-center">
+        <footer className="flex justify-center items-center p-4 text-sm text-white dark:text-gray-300 relative">
+          <div className="text-center">
+            <span>© 2024 Free Daily Motivation.</span>
+            <br />
+            <span>All rights reserved.</span>
+          </div>
+          <div className="absolute right-4">
             <Link href="https://www.facebook.com/people/Free-Daily-Motivation/61566119962164/" className="flex items-center text-blue-600 hover:underline">
               <Facebook className="h-5 w-5 mr-1" />
               Join the Community
