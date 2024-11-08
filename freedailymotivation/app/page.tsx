168b0af3eb1b9d302f40e366706e23cb7788e50a
Miguel Joao Macedo Parente <miguel.joao.parente@outlook.com>
2024-11-08 15:27:40 +0100

    improve landing page

--- freedailymotivation/app/page.tsx
+++ freedailymotivation/app/page.tsx
@@ -18,13 +18,13 @@ export default function Home() {
       <div className="min-h-screen flex flex-col">
         <main className="container mx-auto px-4 py-12 md:py-8 flex-grow">
           <div className="mb-16 md:mb-12">
-            <h1 className={`${poppins.className} text-[32px] md:text-[42px] font-bold mb-8 text-[rgb(51,51,51)] dark:text-white text-center`}>
+            <h1 className={`${poppins.className} text-[32px] md:text-[5 2px] font-bold mb-8 text-[rgb(51,51,51)] dark:text-white text-center`}>
               Free Daily Motivation
             </h1>
-            <p className="text-lg text-center mb-4">Find the top inspirational quotes from famous people</p>
+            <p className="text-lg text-center mb-4 italic">Find the best inspirational quotes from famous and influential people across various fields, including business, sports, science, and life, to motivate and inspire you daily.</p>
             <div className="flex justify-center">
               <Link href="/find-quotes">
-                <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+                <button className="px-4 py-2 bg-gradient-to-r from-blue-500 to-purple-600 text-white rounded hover:from-blue-600 hover:to-purple-700">
                   Find Quotes
                 </button>
               </Link>
