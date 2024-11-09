1428ab510d9a978d5ef9bcce42a6203abd6cc59f
Miguel Joao Macedo Parente <miguel.joao.parente@outlook.com>
2024-11-09 23:50:10 +0100

    subtitle main page style

--- freedailymotivation/app/page.tsx
+++ freedailymotivation/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             <h1 className={`${poppins.className} text-[32px] md:text-[42px] lg:text-[52px] font-bold mb-8 text-[rgb(51,51,51)] dark:text-white`}>
               Free Daily Motivation
             </h1>
-            <p className="text-lg mb-4 italic">Find the best inspirational quotes from famous and influential people across various fields, including business, sports, science, and life, to motivate and inspire you daily.</p>
+            <p className="text-lg mb-4 text-gray-500 dark:text-gray-400">Find the best inspirational quotes from famous and influential people across various fields, including business, sports, science, and life, to motivate and inspire you daily.</p>
             <div className="flex justify-center">
               <Link href="/find-quotes">
                 <button className="px-4 py-2 bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 text-white rounded hover:from-pink-600 hover:via-purple-600 hover:to-indigo-600">
