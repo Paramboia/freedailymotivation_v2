a3083ff4719bbb4a285098625320d28f87bd358f
Miguel Joao Macedo Parente <miguel.joao.parente@outlook.com>
2024-11-09 16:02:17 +0100

    loading missing style

--- freedailymotivation/app/find-quotes/page.tsx
+++ freedailymotivation/app/find-quotes/page.tsx
@@ -98,10 +98,31 @@ export default function FindQuotes() {
             />
           )}
           {!quote && (
-            <div className="flex justify-center items-center h-64">
+            <div className="flex items-center justify-center min-h-screen">
+            <div className="flex flex-col items-center -mt-16"> {/* Adjust the margin-top here */}
               <div className="loader"></div>
-              <div>Loading quote...</div>
+              <div>Finding the best motivational quotes for you...</div>
             </div>
+    
+            {/* Add the loader styles */}
+            <style jsx>{`
+              .loader {
+                border: 8px solid transparent;
+                border-top: 8px solid;
+                border-radius: 50%;
+                width: 40px;
+                height: 40px;
+                animation: spin 1s linear infinite;
+                border-image: linear-gradient(to right, #ff007f, #3498db);
+                border-image-slice: 1;
+              }
+    
+              @keyframes spin {
+                0% { transform: rotate(0deg); }
+                100% { transform: rotate(360deg); }
+              }
+            `}</style>
+          </div>
           )}
           {categories.length > 0 && (
             <div className="mt-8">
