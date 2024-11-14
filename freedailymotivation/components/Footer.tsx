6a0238728e9606733260c84893bc2890662f110d
Miguel Joao Macedo Parente <miguel.joao.parente@outlook.com>
2024-11-14 15:14:15 +0100

    improve footer

--- freedailymotivation/components/Footer.tsx
+++ freedailymotivation/components/Footer.tsx
@@ -12,15 +12,15 @@ const Footer = () => {
         <span>All rights reserved.</span>
       </div>
       <div className="absolute right-4 flex items-center">
-        <Link href="https://www.facebook.com/people/Free-Daily-Motivation/61566119962164/" className="flex items-center text-white-600 hover:underline">
+        <Link href="https://www.facebook.com/people/Free-Daily-Motivation/61566119962164/" className="flex items-center text-blue-600 hover:underline">
           <Facebook className="h-5 w-5 mr-1" />
           <span className="hidden sm:inline">Join the Community</span>
         </Link>
       </div>
       <div className="absolute left-4 flex items-center">
         <Link href="/about" className="flex items-center">
-          <span className="hidden sm:inline text-white-600 hover:underline">About Us</span>
-          <Info className="h-5 w-5 mr-1 text-blue-600 hover:underline sm:hidden" />
+          <Info className="h-5 w-5 mr-1 text-blue-600 hover:underline" />
+          <span className="hidden sm:inline text-blue-600 hover:underline">About Us</span>
         </Link>
       </div>
       <div className="absolute left-20 flex items-center">
