57e18122c696d4b24efc4fe7a041fbbd80f5bf5e
Miguel Joao Macedo Parente <miguel.joao.parente@outlook.com>
2024-11-14 15:07:09 +0100

    improve footer

--- freedailymotivation/components/Footer.tsx
+++ freedailymotivation/components/Footer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Facebook } from 'lucide-react';
+import { Facebook, Info } from 'lucide-react';
 import Link from 'next/link';
 
 const Footer = () => {
@@ -12,14 +12,21 @@ const Footer = () => {
         <span>All rights reserved.</span>
       </div>
       <div className="absolute right-4 flex items-center">
-        <Link href="https://www.facebook.com/people/Free-Daily-Motivation/61566119962164/" className="flex items-center text-blue-600 hover:underline">
+        <Link href="https://www.facebook.com/people/Free-Daily-Motivation/61566119962164/" className="flex items-center text-white-600 hover:underline">
           <Facebook className="h-5 w-5 mr-1" />
           <span className="hidden sm:inline">Join the Community</span>
         </Link>
       </div>
       <div className="absolute left-4 flex items-center">
         <Link href="/about" className="flex items-center">
-          <span className="text-blue-600 hover:underline">About Us</span>
+          <span className="hidden sm:inline text-white hover:text-white/80 focus:text-white/80 bg-transparent hover:bg-transparent focus:bg-transparent dark:text-gray-300 dark:hover:text-white dark:focus:text-whit">About Us</span>
+          <Info className="h-5 w-5 mr-1 text-white-600 hover:underline sm:hidden" />
+        </Link>
+      </div>
+      <div className="absolute left-20 flex items-center">
+        <Link href="/info" className="flex items-center">
+          <Info className="h-5 w-5 mr-1 text-white hover:text-white/80 focus:text-white/80 bg-transparent hover:bg-transparent focus:bg-transparent dark:text-gray-300 dark:hover:text-white dark:focus:text-whit" />
+          <span className="hidden sm:inline">Info</span>
         </Link>
       </div>
     </footer>
