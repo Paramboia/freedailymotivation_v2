1573480c141bad426d0f6a54b6ad2532424cc85d
Miguel Joao Macedo Parente <miguel.joao.parente@outlook.com>
2024-11-15 00:11:07 +0100

    fix vercel deploy

--- freedailymotivation/app/favorite-quotes/page.tsx
+++ freedailymotivation/app/favorite-quotes/page.tsx
@@ -2,11 +2,9 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import Link from 'next/link';
 import { Button } from "@/components/ui/button";
-import ThemeWrapper from "@/components/ThemeWrapper";
 import { Quote } from '@/types';
 import { Metadata } from 'next';
 import { Poppins } from "next/font/google";
-import Footer from "@/components/Footer"; // Import the Footer component
 
 const poppins = Poppins({
   weight: ['700'],
@@ -113,47 +111,39 @@ export default async function FavoriteQuotes() {
   console.log('Quotes retrieved:', quotes); // Log the quotes array obtained
 
   return (
-    // <ThemeWrapper>
-      <div className="min-h-screen flex flex-col">
-        <main className="flex-grow flex flex-col items-center justify-center p-8">
-          <h1
-            className={`${poppins.className} text-[32px] md:text-[42px] lg:text-[52px] font-bold mb-8 text-[rgb(51,51,51)] dark:text-white text-center`}
+    <div className="min-h-screen flex flex-col">
+      <main className="flex-grow flex flex-col items-center justify-center p-8">
+        <h1
+          className={`${poppins.className} text-[32px] md:text-[42px] lg:text-[52px] font-bold mb-8 text-[rgb(51,51,51)] dark:text-white text-center`}
+        >
+          My Favorite Quotes
+        </h1>
+        <div className="w-full max-w-4xl mx-auto flex flex-col items-center justify-center">
+          {quotes.length > 0 ? (
+            quotes.map((quote: Quote) => (
+              <div key={quote.id} className="mb-4">
+                <p className="text-lg dark:text-gray-300">
+                  "{quote.text}" - {quote.author}
+                </p>
+              </div>
+            ))
+          ) : (
+            <p className="dark:text-gray-300">
+              You have no favorite quotes yet.
+            </p>
+          )}
+        </div>
+        <Link href="/" className="mt-8">
+          <Button
+            variant="secondary"
+            className="dark:bg-[#333] dark:text-white dark:hover:bg-[#444]"
           >
-            My Favorite Quotes
-          </h1>
-          <div className="w-full max-w-4xl mx-auto flex flex-col items-center justify-center">
-            {quotes.length > 0 ? (
-              quotes.map((quote: Quote) => (
-                <div key={quote.id} className="mb-4">
-                  <p className="text-lg dark:text-gray-300">
-                    "{quote.text}" - {quote.author}
-                  </p>
-                </div>
-              ))
-            ) : (
-              <p className="dark:text-gray-300">
-                You have no favorite quotes yet.
-              </p>
-            )}
-          </div>
-          <Link href="/" className="mt-8">
-            <Button
-              variant="secondary"
-              className="dark:bg-[#333] dark:text-white dark:hover:bg-[#444]"
-            >
-              Back to Home
-            </Button>
-          </Link>
-        </main>
-        {/* <Footer /> */}
-      </div>
-    // </ThemeWrapper>
+            Back to Home
+          </Button>
+        </Link>
+      </main>
+    </div>
   );
 }
 
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: 'Favorite Quotes | Free Daily Motivation',
-    description: 'View your favorite quotes that inspire and motivate you.',
-  };
-}
+export async function gen
