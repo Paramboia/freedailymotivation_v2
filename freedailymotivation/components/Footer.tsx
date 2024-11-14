5910cf348a4fffad629af86d2c5d8920273ec5d0
Miguel Joao Macedo Parente <miguel.joao.parente@outlook.com>
2024-11-14 15:21:56 +0100

    footer improve hover effect

--- freedailymotivation/components/Footer.tsx
+++ freedailymotivation/components/Footer.tsx
@@ -12,15 +12,15 @@ const Footer = () => {
         <span>All rights reserved.</span>
       </div>
       <div className="absolute right-4 flex items-center">
-        <Link href="https://www.facebook.com/people/Free-Daily-Motivation/61566119962164/" className="flex items-center text-white dark:text-gray-300 hover:underline">
+        <Link href="https://www.facebook.com/people/Free-Daily-Motivation/61566119962164/" className="flex items-center text-white dark:text-gray-300 hover:text-white/80 focus:text-white/80">
           <Facebook className="h-5 w-5 mr-1" />
           <span className="hidden sm:inline">Join the Community</span>
         </Link>
       </div>
       <div className="absolute left-4 flex items-center">
         <Link href="/about" className="flex items-center">
-          <Info className="h-5 w-5 mr-1 text-white dark:text-gray-300 hover:underline" />
-          <span className="hidden sm:inline text-white dark:text-gray-300 hover:underline">About Us</span>
+          <Info className="h-5 w-5 mr-1 text-white dark:text-gray-300 hover:text-white/80 focus:text-white/80" />
+          <span className="hidden sm:inline text-white dark:text-gray-300 hover:text-white/80 focus:text-white/80">About Us</span>
         </Link>
       </div>
     </footer>
