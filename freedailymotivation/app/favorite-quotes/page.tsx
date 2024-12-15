abc314574b43b0929c748e2252fa691c851b4c16
Miguel Joao Macedo Parente <miguel.joao.parente@outlook.com>
2024-12-15 15:28:54 +0100

    issue favorite quotes

--- freedailymotivation/app/favorite-quotes/page.tsx
+++ freedailymotivation/app/favorite-quotes/page.tsx
@@ -7,6 +7,8 @@ import { Quote } from '@/types';
 import { Metadata } from 'next';
 import { Poppins } from "next/font/google";
 import Footer from "@/components/Footer"; // Import the Footer component
+import dynamic from 'next/dynamic';
+const QuoteBox = dynamic(() => import("@/components/quote-box"), { ssr: false });
 
 const poppins = Poppins({
   weight: ['700'],
@@ -33,37 +35,32 @@ async function getUserId(clerkUserId: string) {
 async function getFavoriteQuotes(userId: string) {
   const supabase = createServerComponentClient({ cookies });
   const { data, error } = await supabase
-  .from('favorites')
-  .select(`
-    quote_id,
-    quotes (
-      id,
-      quote_text,
-      authors (
-        author_name
+    .from('favorites')
+    .select(`
+      quote_id,
+      quotes!inner (
+        id,
+        quote_text,
+        authors!inner (
+          author_name
+        )
       )
-    )
-  `)
-  .eq('user_id', userId);
+    `)
+    .eq('user_id', userId);
 
   if (error) {
     console.error('Error fetching favorite quotes:', error);
     return [];
   }
 
-  return data.map((item) => {
-    const quote = Array.isArray(item.quotes) ? item.quotes[0] : item.quotes;
-    const author = quote?.authors ? quote.authors[0] : { author_name: 'Unknown Author' };
-    
-    return {
-      id: quote?.id || item.quote_id,
-      text: quote?.quote_text || 'Unknown Quote',
-      author: author?.author_name || 'Unknown Author',
-      likes: 0,
-      category: '',
-      dislikes: 0,
-    };
-  });
+  return data.map((item) => ({
+    id: item.quotes.id,
+    text: item.quotes.quote_text,
+    author: item.quotes.authors.author_name,
+    likes: 0,
+    category: '',
+    dislikes: 0,
+  }));
 }
 
 export default async function FavoriteQuotes() {
@@ -126,11 +123,11 @@ export default async function FavoriteQuotes() {
           <div className="w-full max-w-4xl mx-auto flex flex-col items-center justify-center">
             {quotes.length > 0 ? (
               quotes.map((quote: Quote) => (
-                <div key={quote.id} className="mb-4">
-                  <p className="text-lg dark:text-gray-300">
-                    "{quote.text}" - {quote.author}
-                  </p>
-                </div>
+                <QuoteBox
+                  key={quote.id}
+                  quote={quote}
+                  _isAuthorPage={true}
+                />
               ))
             ) : (
               <p className="dark:text-gray-300">
