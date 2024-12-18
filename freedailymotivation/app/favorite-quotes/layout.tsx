6eaf0f5b2cf84094e5d9140c8475e4451733277f
Miguel Joao Macedo Parente <miguel.joao.parente@outlook.com>
2024-12-18 13:52:12 +0100

    fix vercel deploy

--- freedailymotivation/app/favorite-quotes/layout.tsx
+++ freedailymotivation/app/favorite-quotes/layout.tsx
@@ -1,6 +1,15 @@
 import { Metadata } from 'next';
+import React from 'react';
 
 export const metadata: Metadata = {
   title: 'Your Favorite Quotes - Free Daily Motivation',
   description: 'View your collection of favorite motivational quotes.',
 };
+
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
+}
