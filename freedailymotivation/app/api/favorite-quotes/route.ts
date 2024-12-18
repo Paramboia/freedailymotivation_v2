4d094910179b833dce860cc7ad56308b2e63dee7
Miguel Joao Macedo Parente <miguel.joao.parente@outlook.com>
2024-12-18 22:18:40 +0100

    fix vercel deploy

--- freedailymotivation/app/api/favorite-quotes/route.ts
+++ freedailymotivation/app/api/favorite-quotes/route.ts
@@ -1,13 +1,12 @@
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
-import { auth } from '@clerk/nextjs';
-import { type NextRequest } from 'next/server';
+import { getAuth } from '@clerk/nextjs/server';
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   try {
     // Get the user ID from Clerk
-    const { userId } = auth();
+    const { userId } = getAuth();
     
     if (!userId) {
       return new NextResponse(
