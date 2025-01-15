34dd9416dca508fdd312e206b3d789e55c2fb2e4
Miguel Joao Macedo Parente <miguel.joao.parente@outlook.com>
2025-01-15 22:32:07 +0100

    issue "Unknown Author" favorite-quotes

--- freedailymotivation/app/api/favorite-quotes/route.ts
+++ freedailymotivation/app/api/favorite-quotes/route.ts
@@ -135,7 +135,8 @@ export async function GET() {
       .select(`
         id,
         quote_text,
-        authors:authors!inner (
+        author_id,
+        authors!quotes_author_id_fkey (
           author_name
         )
       `)
